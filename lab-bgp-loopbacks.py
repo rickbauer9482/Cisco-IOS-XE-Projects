98375c8e0b3bd7719fd05123f79ffbfa7739eff9
Rick Bauer <rickbaue@cisco.com>
2020-10-20 14:58:29 -0400

    Update lab-bgp-loopbacks.py

--- lab-bgp-loopbacks.py
+++ lab-bgp-loopbacks.py
@@ -5,7 +5,6 @@ from random import randint
 import ipaddress 
 
 # Variables
-
 max_mask = 27
 max_ip = 254
 class_max_a = 126
@@ -28,6 +27,7 @@ c = 0
 bgp_list = []
 interface_list = []
 
+# work to be done - randomly generate Internet Address and Subnet
 with open(str(file_name) + '.txt', 'w') as file1:
     while i <= n:
         oct_1 = randint(1, class_max_c)
@@ -35,9 +35,9 @@ with open(str(file_name) + '.txt', 'w') as file1:
         oct_3 = randint(1, max_ip)
         oct_4 = randint(1, max_ip)
 
-        if oct_1 < class_max_a:
+        if oct_1 < class_max_a and oct_1 != 127:
             mask = randint(9, max_mask)
-        elif oct_1 < class_max_b:
+        elif oct_1 < class_max_b and oct_1 != 127:
             mask = randint(17, max_mask)
         else:
             mask = randint(25, max_mask)
